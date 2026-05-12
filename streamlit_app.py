66f849055b56f74ae76bdb2e90b56e0fe943cba2
4rthur-web <toir2101@gmail.com>
2026-05-12 09:00:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Kalkulator Pahalaa")
+st.title("🎈 Kalkulator Pahalaaaa")
 st.write(
     "Anda merasa sombong karna pahala anda banyak?, ayo cek pahala anda di [docs.streamlit.io](https://docs.streamlit.io/)."
 )
